38561da6c1f192bc0d47082aa9fd9be57fa4e186
eugene.sokolov <eugene.sokolov@techery.io>
2022-03-04 13:41:53 +0200

    add health and logger

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common'
+import { HealthModule } from './health/health.module'
+import { LoggerModule } from './logger/logger.module'
 
 @Module({
-  imports: []
+  imports: [LoggerModule, HealthModule]
 })
 export class CoreModule {}
--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,20 @@ import { json, urlencoded } from 'express'
 import helmet from 'helmet'
 import { AppModule } from './app.module'
 import { AppConfig } from './config/app.config'
+import { LoggerService } from './core/logger/logger.service'
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule, { cors: true })
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    bufferLogs: true
+  })
   const config = app.get(AppConfig)
+  const logger = app.get(LoggerService)
 
   app.use(helmet())
   app.use(json({ limit: config.jsonLimit }))
   app.use(urlencoded({ extended: true, limit: config.urlLimit }))
+  app.useLogger(logger)
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix(config.base)
   await app.listen(config.port)
