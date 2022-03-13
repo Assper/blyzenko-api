fe51bdde4dbe347f7f879950e91f740cfe4d140e
eugene.sokolov <eugene.sokolov@techery.io>
2022-03-13 11:46:13 +0200

    refact notify

--- src/core/notify/notify.controller.ts
+++ src/core/notify/notify.controller.ts
@@ -1,5 +1,7 @@
 import { Controller } from '@nestjs/common'
 import { AuthSlot } from '../auth/auth.slot'
+import { ConfirmTokenDocument } from '../auth/schemas/confirm-token.schema'
+import { LoggerService } from '../logger/logger.service'
 import { NotifyService } from './notify.service'
 import { SmsPhonePipe } from './sms-phone.pipe'
 
@@ -7,17 +9,25 @@ import { SmsPhonePipe } from './sms-phone.pipe'
 export class NotifyController {
   constructor(
     private readonly service: NotifyService,
-    private readonly authSlot: AuthSlot
+    private readonly authSlot: AuthSlot,
+    private readonly logger: LoggerService
   ) {
     this.init()
   }
 
   private init(): void {
-    this.authSlot.confirm().subscribe(async (confirm) => {
-      if (!confirm) return
-      const { phone, token } = confirm
-      const validPhone = new SmsPhonePipe().transform(phone)
-      await this.service.sendAuthConfirm(token, validPhone)
-    })
+    this.authSlot.confirm().subscribe(this.sendSmsCode.bind(this))
+  }
+
+  private async sendSmsCode(confirm?: ConfirmTokenDocument): Promise<boolean> {
+    if (!confirm) {
+      this.logger.log(
+        `NotifyController (sendSmsCode): confirm token not emmited`
+      )
+      return
+    }
+    const { phone, token } = confirm
+    const validPhone = new SmsPhonePipe().transform(phone)
+    return await this.service.sendAuthConfirm(token, validPhone)
   }
 }
